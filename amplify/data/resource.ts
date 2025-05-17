8862c2ab6f17fc082e3cec750c84dff8ffc5cff4
makoto.sagara <sugary.macarons@gmail.com>
2025-05-17 13:46:05 +0900

    Add Table

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -12,6 +12,15 @@ const schema = a.schema({
       content: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
+  Talent: a.
+    model({
+      name: a.string(),
+      description: a.string(),
+      image: a.string(),
+      price: a.number(),
+      category: a.string(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
